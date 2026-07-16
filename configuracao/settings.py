6575b944e461298ba696e038035b252a666f06ba
Yukilow-ops <enzoyps@gmail.com>
2026-07-15 22:23:41 -0300

    Adicionando script de setup e correções

--- configuracao/settings.py
+++ configuracao/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -75,10 +77,11 @@ WSGI_APPLICATION = 'configuracao.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
